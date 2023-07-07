3b87c36cc4d53f40cb57c4942d6d541219bf7f4f
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-07-07 16:41:26 +0700

    add default layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,30 @@
+import styles from './DefaultLayout.scss'
+import classNames from 'classnames';
+import logo from '~/components/assets/image/gunther.jpg'
+
+
+const cx = classNames.bind(styles)
+
+function DefaultLayout() {
+    return (
+        <div className={cx("dWrapper")}>
+            <div className={cx("dContent")}>
+                <div className={cx("dLeftContainer")}>
+                    <div className={cx("LogoBorder")}>
+                        <img src={logo} alt='LOGO'></img>
+                    </div>
+                    <div className={cx("dItem")}>Trang Chủ</div>
+                    <div className={cx("dItem")}>Thực Đơn</div>
+                    <div className={cx("dItem")}>Hoá Đơn</div>
+                </div>
+                <div className={cx("dRightContainer")}>
+                    <div className={cx("UserName dItem")}></div>
+                    <div className={cx("dLogin")}>Đăng Nhập</div>
+                    <div className={cx("dLogout")}>Đăng Xuất</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
