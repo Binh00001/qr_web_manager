9505cd5f4c30022619a700b5657931140d011370
Tran Huy <tranxuanhuy3201@gmail.com>
2023-07-08 19:29:29 +0700

    addLogic

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,23 +1,25 @@
-
-
-import { Fragment } from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import classNames from "classnames";
 import styles from '~/pages/menu/menu.scss';
-
-import gunther from '~/components/assets/image/gunther.jpg'
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles)
 
 function Menu() {
     const [category, setCategory] = useState([]);
     const [listDish, setListDish] = useState([]);
+    const [type, setType] = useState(null);
+    const navigate = useNavigate();
+
     useEffect(() => {
         axios
             .get("http://117.4.194.207:3003/category/all")
             .then((response) => {
-                setCategory(response.data);
+                const data = response.data;
+                setCategory(data);
+                // setType(data[0]);
+
             })
             .catch((error) => {
                 console.log(error);
@@ -39,21 +41,27 @@ function Menu() {
             <div className={cx("blackBar")}>
                 <div className={cx("TopBar")}>
                     <div className={cx("mTopBar")}>
-                        <div className={cx("mText")}>Quản Lý Thực Đơn</div>
+                        <div className={cx("mText active")}>Quản Lý Thực Đơn</div>
                         <div className={cx("mText")}>Các Món Đã Ẩn</div>
-                        <div className={cx("mText")}>Thêm Món Ăn</div>
+                        <div className={cx("mText")} onClick={() => navigate('/addDish')}>Thêm Món Ăn</div>
                     </div>
                 </div>
             </div>
             <div className={cx("mBody")}>
                 <div className={cx("mNavBar")}>
-                    <button className={cx("mNavButton")}>Tất Cả</button>
+                    <button className={cx("mNavButton", { active: type === null })} onClick={() => setType(null)}>Tất Cả</button>
+                    <button className={cx("mNavButton", { active: type === "bestseller" })} onClick={() => setType('bestseller')}>Bestseller</button>
                     {category.map((cat, index) => (
-                        <button className={cx("mNavButton")}>{cat.name}</button>
+                        <button key={index} className={cx("mNavButton", { active: type === cat })} onClick={() => setType(cat)}>{cat.name}</button>
                     ))}
                 </div>
                 <div className={cx("mContent")}>
-                    {listDish.map((food, index) => (
+                    {(type === null
+                        ? listDish
+                        : type === 'bestseller'
+                            ? listDish.filter((dish) => dish.isBestSeller)
+                            : listDish.filter((dish) => dish.category === type.name)
+                    ).map((food, index) => (
                         <div
                             key={index}
                             className={cx("mItem")}>
@@ -62,7 +70,7 @@ function Menu() {
                             </div>
                             <div className={cx("mItemInfo")}>
                                 <div className={cx("mName")}>{food.name}</div>
-                                <div className={cx("mPrice")}>{food.price}đ</div>
+                                <div className={cx("mPrice")}>{food.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</div>
                                 <div className={cx("mQuantity")}>Số lượng: {food.amount}</div>
                             </div>
                         </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ export const publicRoutes = [
 //   { path: "/home/:table", component: Home, layout: null },
   { path: "/", component: Home },
   { path: "/menu", component: Menu},
-  { path: "/adddish", component: AddDish},
+  { path: "/addDish", component: AddDish},
 //   { path: "/cart", component: Cart, layout: null },
 //   { path: "/showall", component: ShowAll, layout: null },
 
