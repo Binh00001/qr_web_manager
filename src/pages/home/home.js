e24be71f0039dc168e9ba84f63b5c7a1404cd644
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-07-11 16:57:13 +0700

    add reddot and sound to new notification

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,12 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useRef } from "react";
 import classNames from "classnames";
 import styles from "~/pages/home/home.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import tabelNonActive from "~/components/assets/image/table_and_chair_non_active.png";
 import tabelActive from "~/components/assets/image/table_and_chair_active.png";
 import moment from "moment";
+import ting from "~/components/assets/sound/Herta Kurukuru Kururin 1 (mp3cut.net) (5).mp3"
 import "moment/locale/vi";
 
 const cx = classNames.bind(styles);
@@ -14,52 +15,89 @@ function Home() {
   const [tables, setTables] = useState([]);
   const [requests, setRequests] = useState([]);
   const [isAllActive, setIsAllActive] = useState(true);
-  // const [activeTable, setActiveTable] = useState(null);
+  const [isNewRequest, setIsNewRequest] = useState(false);
+  const [oldRequest, setOldRequest] = useState([]);
+  const audioRef = useRef(null);
+
 
   useEffect(() => {
     axios
       .get("http://117.4.194.207:3003/table/all")
       .then((response) => {
-        // console.log(response);
         setTables(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
   useEffect(() => {
     const fetchData = () => {
       axios
         .get("http://117.4.194.207:3003/call-staff/all?time=60")
         .then((response) => {
-          // console.log(response);
-          setRequests(response.data);
+          const newRequests = response.data;
+          setRequests(newRequests); 
+          
         })
         .catch((error) => {
           console.log(error);
         });
     };
+
     fetchData();
 
-    // Thiết lập interval để gọi lại fetchData() sau mỗi 1 phút
     const interval = setInterval(() => {
       fetchData();
-    }, 30000); // 1 phút = 60000 milliseconds
-    // Xóa interval khi component unmount
+    }, 10000);
+
     return () => {
       clearInterval(interval);
     };
   }, []);
+
+  useEffect(() => {
+    if (requests.length > 0) {
+      const { _id } = requests[0];
+      setOldRequest(_id);
+    }
+  }, [requests]);
+
+  useEffect(() => {
+    playSound()
+  }, [oldRequest]);
+  
+
+  const playSound = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }
+
+
+
+  const removeRedDot = (request) => {
+    const requestTime = moment(request.createdAt, "DD/MM/YYYY, HH:mm:ss");
+    const currentTime = moment();
+    const timeDifference = moment
+      .duration(currentTime.diff(requestTime))
+      .asMinutes();
+    if (timeDifference <= 1) {
+      // request.showRedDot = true;
+      return true;
+    } else {
+      // request.showRedDot = false; 
+      return false
+    }
+  };
+
   const clickHandler = (table) => {
     const updatedTable = {
-      // ...table,
       isActive: !table.isActive,
     };
-    console.log(updatedTable);
     axios
       .put(`http://117.4.194.207:3003/table/active/${table.name}`, updatedTable)
       .then((response) => {
-        console.log(response);
         const updatedTable = response.data;
         setTables((prevTables) => {
           const updatedTables = prevTables.map((prevTable) => {
@@ -75,35 +113,32 @@ function Home() {
         console.log(error);
       });
   };
+
   const isWithin5Minutes = (createdAt) => {
-    const requestTime = moment(createdAt, "DD/MM/YYYY, HH:mm:ss"); // Chuyển đổi chuỗi thành đối tượng Moment và định dạng theo mẫu 'DD/MM/YYYY, HH:mm:ss'
-    const currentTime = moment(); // Lấy thời gian hiện tại
-    console.log(requestTime);
+    const requestTime = moment(createdAt, "DD/MM/YYYY, HH:mm:ss");
+    const currentTime = moment();
     const timeDifference = moment
       .duration(currentTime.diff(requestTime))
-      .asMinutes(); // Tính khoảng thời gian trong phút
+      .asMinutes();
 
-    return timeDifference <= 10; // Trả về true nếu trong vòng 5 phút, ngược lại trả về false
+    return timeDifference <= 10;
   };
 
-  // console.log(activeTable);
   if (!tables || !requests) {
     return <div>Loading...</div>;
   }
+
   return (
     <Fragment>
+
+      <audio ref={audioRef}>
+        <source src={ting} type="audio/mpeg" />
+      </audio>
+
       <div className={cx("Wrapper")}>
         <div className={cx("blackBar")}>
           <div className={cx("TopBar")}>
             <div className={cx("hLeftContainer")}>
-              <div className={cx("hText")}>Quản Lý Bàn Ăn:</div>
-              <div className={cx("hAllActiveGroup")}>
-                {/* {!isAllActive && <div className={cx("hText nonActive")}>Bật Tất Cả</div>} */}
-                {/* {isAllActive && <div className={cx("hText isActive")}>Bật Tất Cả</div>} */}
-                {/* <button className={cx("hAllActiveButton")}>CLICK</button> */}
-              </div>
-            </div>
-            <div className={cx("hRightContainer")}>
               <div className={cx("hText")}>
                 Quản Lý Yêu Cầu(sau 5 phút yêu cầu sẽ bị ẩn):
               </div>
@@ -129,11 +164,12 @@ function Home() {
           <div className={cx("hRightContainer")}>
             <div className={cx("hAllNotification")}>
               {requests
-                .filter((request) => isWithin5Minutes(request.createdAt)) // Lọc danh sách yêu cầu trong vòng 5 phút
+                .filter((request) => isWithin5Minutes(request.createdAt))
                 .map((request, index) => (
                   <div key={index} className={cx("hNotification")}>
                     <div>Bàn {request.table}</div>
                     <div>{moment(request.createdAt).format("h:mm A")}</div>
+                    <div className={cx("redDot", { "redDotHided": !removeRedDot(request) })}></div>
                   </div>
                 ))}
             </div>
